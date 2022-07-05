aa537ae428a468ad46da52efc94d2b8976be49c9
Franklin Eduardo <fhcdudu13@gmail.com>
2022-07-05 17:31:34 -0300

    Dados do controller da tela gráfica inseridos

--- Calculadora/src/TelaInicialController.java
+++ Calculadora/src/TelaInicialController.java
@@ -1,3 +1,49 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class TelaInicialController {
-    
+
+    @FXML
+    private Button btnNUM0;
+
+    @FXML
+    private Button btnDiv;
+
+    @FXML
+    private Button btnSoma;
+
+    @FXML
+    private Button btnNUM9;
+
+    @FXML
+    private Button btnNUM8;
+
+    @FXML
+    private Button btnNUM7;
+
+    @FXML
+    private Button btnNUM6;
+
+    @FXML
+    private Button btnNUM5;
+
+    @FXML
+    private Button btnNUM4;
+
+    @FXML
+    private Button btnNUM3;
+
+    @FXML
+    private Button btnNUM2;
+
+    @FXML
+    private Button btnMulti;
+
+    @FXML
+    private Button btnSub;
+
+    @FXML
+    private Label telacalculos;
+
 }
